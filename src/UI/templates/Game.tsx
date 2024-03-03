646c8cbb63b5d119ab4fee314feb5caf25da5c4c
kinxyzz <kinxyzz@gmail.com>
2024-03-03 19:28:13 +0700

    'firstcommit'

--- src/UI/templates/Game.tsx
+++ src/UI/templates/Game.tsx
@@ -6,6 +6,7 @@ export default function Game() {
   const [start, setStart] = useState(false);
   const [status, setStatus] = useState<string | null>(null);
   const [count, setCount] = useState(10);
+  const [name, setName] = useState<string | null>(null);
   const [shuffledArray, setShuffledArray] = useState(() => {
     const shuffled = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     return shuffled.sort(() => Math.random() - 0.5);
@@ -65,61 +66,112 @@ export default function Game() {
     }, 2000);
   }
 
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    setName(event.target.name.value);
+  }
+
   return (
-    <div className="flex justify-center bg-red-900 h-screen">
-      <div className="bg-blue-900 w-[480px]">
-        <div className="flex justify-center p-2 text-white">Memory Game</div>
-
-        <div className="relative flex flex-col h-full items-center text-white bg-gray-900">
-          <div className="p-20">
-            <h2>Timer : </h2>
-          </div>
-          <p className="">{status}</p>
-          <p className="mb-10 mt-20">Point : {poin}</p>
-          <div className="grid grid-cols-3 gap-4">
-            {shuffledArray.map((item) => (
+    <>
+      {!name && (
+        <div className="z-[999] fixed flex justify-center items-center h-screen w-full bg-black/80">
+          <div className="bg-white h-1/5 p-2 w-5/6 rounded-md">
+            <h2>Your Name:</h2>
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+              <input
+                className="p-2 border-2 border-slate-900 rounded-sm"
+                type="text"
+                name="name"
+                placeholder="your name..."
+              />
               <button
-                key={item}
-                onClick={() => handleNumber(item)}
-                disabled={count === 0 ? false : true}
-                className={`w-20 h-20 ${
-                  newArr.includes(item) && isCorrectOrder
-                    ? "bg-green-500"
-                    : "bg-cyan-500"
-                }`}
+                className="px-4 py-2 bg-green-500 rounded-md"
+                type="submit"
               >
-                {count === 0 ? "?" : item}
+                Submit
               </button>
-            ))}
+            </form>
           </div>
-          {count === 0 && (
-            <button
-              onClick={handleReshuffle}
-              className="mt-10 bg-blue-500 p-1 px-3 rounded-md"
-            >
-              Reshuffle
-            </button>
-          )}
+        </div>
+      )}
+      <div className="flex justify-center bg-red-900 h-screen">
+        <div className="bg-gray-900 w-[480px] container">
+          <div className="flex justify-center p-2 text-white">Memory Game</div>
+          <div className="Profile text-white p-4">
+            <div className="border-b-2 border-slate-200">
+              <h2>Name:{name}</h2>
+              <p className="">Point : {poin}</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center text-white bg-gray-900">
+            <div className="p-10">
+              <h2>Timer : </h2>
+            </div>
+            <p className="">{status || "-"}</p>
 
-          <div className="absolute bottom-64 flex flex-col items-center gap-4">
-            {!start ? (
-              <>
+            <div className="grid grid-cols-3 gap-4">
+              {shuffledArray.map((item) => (
                 <button
-                  onClick={() => setStart(true)}
-                  className="bg-green-500 px-4 rounded-md"
+                  key={item}
+                  onClick={() => handleNumber(item)}
+                  disabled={count === 0 ? false : true}
+                  className={`w-20 h-20 rounded-md ${
+                    newArr.includes(item) && isCorrectOrder
+                      ? "bg-green-500"
+                      : "bg-cyan-500"
+                  }`}
                 >
-                  Mulai
+                  {count === 0 ? "?" : item}
                 </button>
-                <p>Click start to play</p>
-              </>
-            ) : (
-              <>
-                <p>{count}</p>
-              </>
+              ))}
+            </div>
+            {count === 0 && (
+              <button
+                onClick={handleReshuffle}
+                className="mt-10 bg-blue-500 p-1 px-3 rounded-md"
+              >
+                Reshuffle
+              </button>
             )}
+
+            <div className="flex my-20 flex-col items-center gap-4">
+              {!start ? (
+                <>
+                  <button
+                    onClick={() => setStart(true)}
+                    className="bg-green-500 px-4 py-2 rounded-md"
+                  >
+                    Start
+                  </button>
+                  <p>Click start to play</p>
+                </>
+              ) : (
+                <>
+                  <p>{count}</p>
+                </>
+              )}
+            </div>
+          </div>
+          <div className="p-4">
+            <div className="dropdown dropdown-top">
+              <div tabIndex={0} role="button" className="btn m-1">
+                Leaderboard
+              </div>
+              <ul
+                tabIndex={0}
+                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li>
+                  <a>Item 2</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
